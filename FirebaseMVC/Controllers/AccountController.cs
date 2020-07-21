493c7436179876235aaefdaa0c1e8cb429f6c3c5
Andy Collins <askingalot@gmail.com>
2020-07-21 13:35:11 -0500

    Add project files.

---
+++ FirebaseMVC/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using FirebaseMVC.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirebaseMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Credentials credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(credentials);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ FirebaseMVC/Models/Auth/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirebaseMVC.Models.Auth
+{
+    public class Credentials
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
